26108f7c553da9fccdd077cc674eff7b0356e555
Neeraj Kumar <neerajk.eebitm@gmail.com>
2021-10-13 06:32:30 +0530

    get data impl

---
+++ src/training.py
@@ -0,0 +1,17 @@
+from src.utils.common import read_config
+from src.utils.data_mgmt import get_data
+import argparse
+
+def training(config_path):
+    config = read_config(config_path)
+    
+    validation_datasize = config['params']['validation_datasize']
+    (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+
+    args.add_argument('--config', '-c', default='config.yml')
+
+    parsed_args = args.parse_args()
