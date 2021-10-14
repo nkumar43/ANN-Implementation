a9b5272296811dd1c21631cf7719a1e1ffb68532
Neeraj Kumar <neerajk.eebitm@gmail.com>
2021-10-14 07:04:44 +0530

    updated savemodel

--- src/training.py
+++ src/training.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
 from src.utils.model import create_model, save_model
@@ -25,7 +26,9 @@ def training(config_path):
     artifacts_dir = config['artifacts']['artifacts_dir']
     model_dir = config['artifacts']['model_dir']
     model_dir_path = os.path.join(artifacts_dir, model_dir)
-    os.makedir(model_dir_path, exist_ok=True)
+
+    os.makedirs(model_dir_path, exist_ok=True)
+
     model_name = config['artifacts']['model_name']
     save_model(model, model_name, model_dir)
 
